beb97b5c5062c6b5b35b696c564d113ffab7003e
Marco <marcodimaio2102@gmail.com>
2025-04-12 17:39:23 +0200

    aggiornamento

--- stablediffusion35.py
+++ stablediffusion35.py
@@ -9,11 +9,10 @@ prompt = ("headshot portrait of a young woman, real life, shot on iPhone, realis
 
 negative_prompt = "cartoon, blurry, partial face, deformed, artistic filters, black and white, shadows, cropped head, unrealistic, painting, illustration"
 
-
 image = pipe(
     prompt,
     negative_prompt=negative_prompt,
     num_inference_steps=40,
     guidance_scale=4.5,
 ).images[0]
-image.save("stable-diffusion-3.5-medium.png")
+image.save("stable-diffusion-3.5-medium2.png")
